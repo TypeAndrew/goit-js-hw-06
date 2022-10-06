26cddca3ff983ea40a2ec4dcd7aba7896d7d70d9
TypeAndrew <andy.shular@gmail.com>
2022-10-06 12:09:48 +0300

    last tasks

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6,0)}`;
 }
+
+const bodyEl = document.body;
+const spanEl = document.querySelector("span.color");
+const buttonEl = document.querySelector("button.change-color");
+
+buttonEl.addEventListener("click", (event) => {
+    return spanEl.style.backgroundColor = getRandomHexColor();
+});
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonCreateEl = document.querySelector("button[data-create]");
+const buttonDestroyEl = document.querySelector("button[data-destroy]");
+const inputEl = document.querySelector("input");
+const boxesEl = document.querySelector("div#boxes");
+
+buttonCreateEl.addEventListener("click", (event) => {
+    let size = 30;
+    for (let i = 0; i < Number(inputEl.value); i++) {
+
+        let elBox = document.createElement("div");
+        elBox.style.width = size + "px";
+        elBox.style.height = size + "px";
+        elBox.style.backgroundColor = getRandomHexColor();
+        boxesEl.append(elBox);
+        size += 10;
+    }
+});
+
+buttonDestroyEl.addEventListener("click", (event) => {
+    const rmBoxes = [...boxesEl.childNodes];
+    rmBoxes.forEach(element => {
+        element.remove()
+    })
+});
